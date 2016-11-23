0b4d4e382e0c3faa9504bc94b0168aac4d1d67c7
subenkun <subenkun@gmail.com>
2016-11-23 12:58:42 +0800

    增加app更新接口

--- hot-deploy/cloudcard/src/com/banfftech/cloudcard/CloudCardQueryServices.java
+++ hot-deploy/cloudcard/src/com/banfftech/cloudcard/CloudCardQueryServices.java
@@ -544,4 +544,67 @@ public class CloudCardQueryServices {
 		}
 		return "SUCCESS";
 	}
+	
+	/**
+	 * 查询app版本信息
+	 * @param dctx
+	 * @param context
+	 * @return Map
+	 */
+	public static Map<String, Object> checkAppVersion(DispatchContext dctx, Map<String, Object> context) {
+		String curVersion = (String) context.get("curVersion");
+		String deviceType = (String) context.get("deviceType");
+		String appType = (String) context.get("appType");
+		String latestVersion = null;
+		int updateType = 0;
+
+		String url = null;
+		//判断appType是商家或用户
+		if("biz".equals(appType)){
+			//判断当前设备是ios或android
+			if("ios".equals(deviceType)){
+				if(!"1.0".equals(curVersion)){
+					latestVersion = "1.0";
+					updateType = 0;
+					url = "http://fir.im/6nzd";
+				}else{
+					latestVersion = curVersion;
+				}
+			}else if("android".equals(deviceType)){
+				if(!"1.0".equals(curVersion)){
+					latestVersion = "1.0";
+					updateType = 0;
+					url = "http://fir.im/6nzd";
+				}else{
+					latestVersion = curVersion;
+				}
+			}
+		}else if("user".equals(appType)){
+			//判断当前设备是ios或android
+			if("ios".equals(deviceType)){
+				if(!"1.0".equals(curVersion)){
+					latestVersion = "1.0";
+					updateType = 1;
+					url = "http://fir.im/6nzd";
+				}else{
+					latestVersion = curVersion;
+				}
+			}else if("android".equals(deviceType)){
+				if(!"1.0".equals(curVersion)){
+					latestVersion = "1.0";
+					updateType = 1;
+					url = "http://fir.im/6nzd";
+				}else{
+					latestVersion = curVersion;
+				}
+			}
+		}
+		
+		Map<String, Object> results = ServiceUtil.returnSuccess();
+		results.put("latestVersion", latestVersion);
+		results.put("updateType", updateType);
+		results.put("url", url);
+		return results;
+	}
+
 }
