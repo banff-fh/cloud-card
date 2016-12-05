6d9244195c84cf7df07d0189c1e0d8672893a013
subenkun <subenkun@gmail.com>
2016-12-05 16:53:25 +0800

    修改极光推送key

--- hot-deploy/cloudcard/src/com/banfftech/cloudcard/jpush/JPushServices.java
+++ hot-deploy/cloudcard/src/com/banfftech/cloudcard/jpush/JPushServices.java
@@ -1,17 +1,9 @@
 package com.banfftech.cloudcard.jpush;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
 import org.ofbiz.base.util.Debug;
-import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.util.EntityUtilProperties;
-import org.ofbiz.service.ServiceUtil;
-
-import com.banfftech.cloudcard.CloudCardServices;
 
 import cn.jiguang.common.resp.APIConnectionException;
 import cn.jiguang.common.resp.APIRequestException;
@@ -24,19 +16,24 @@ public class JPushServices {
 	public static final String resourceError = "cloudcardErrorUiLabels";
 
 	//注册jPushClient
-	private static JPushClient getJPushClient(Delegator delegator) {
-		String appkey = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.appkey", delegator);
-		String secret = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.secret", delegator);
-		String expirationTime = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.expirationTime", "172800",delegator);
-		@SuppressWarnings("deprecation")
-		JPushClient jPushClient = new JPushClient(secret, appkey, Integer.valueOf(expirationTime));
-		
+	private static JPushClient getJPushClient(Delegator delegator,String appType) {
+		JPushClient jPushClient = null;
+		if(appType.equals("biz")){
+			String appkey = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.bizAppKey", delegator);
+			String secret = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.bizSecret", delegator);
+			jPushClient = new JPushClient(secret, appkey);
+			return jPushClient;
+		}else if(appType.equals("user")){
+			String appkey = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.userAppKey", delegator);
+			String secret = EntityUtilProperties.getPropertyValue("cloudcard", "jpush.userSecret", delegator);
+			jPushClient = new JPushClient(secret, appkey);
+		}
 		return jPushClient;
 	}
 	
 	// 推送消息
-	public static void pushMessage(Delegator delegator, String title, String msgContent, String registrationID,int seedType) {
-		JPushClient jPushClient = getJPushClient(delegator);
+	public static void pushMessage(Delegator delegator, String title, String msgContent, String registrationID,String appType,int seedType) {
+		JPushClient jPushClient = getJPushClient(delegator,appType);
 		PushResult pushResult = null;
 		try {
 			if (seedType == 0) {
