a4fffbcf109749fe93954d3c169c81edff0438fc
subenkun <subenkun@gmail.com>
2016-12-06 16:55:35 +0800

    修改极光推送推送方式

--- hot-deploy/cloudcard/src/com/banfftech/cloudcard/jpush/JPushServices.java
+++ hot-deploy/cloudcard/src/com/banfftech/cloudcard/jpush/JPushServices.java
@@ -21,6 +21,8 @@ import cn.jiguang.common.resp.APIConnectionException;
 import cn.jiguang.common.resp.APIRequestException;
 import cn.jpush.api.JPushClient;
 import cn.jpush.api.push.PushResult;
+import cn.jpush.api.push.model.Message;
+import cn.jpush.api.push.model.Options;
 import cn.jpush.api.push.model.Platform;
 import cn.jpush.api.push.model.PushPayload;
 import cn.jpush.api.push.model.audience.Audience;
@@ -52,15 +54,15 @@ public class JPushServices {
 	}
 	
 	// 推送消息
-	public static void pushMessage(DispatchContext dctx, Map<String, Object> context) {
+	public static void pushNotifOrMessage(DispatchContext dctx, Map<String, Object> context) {
 		LocalDispatcher dispatcher = dctx.getDispatcher();
 		Delegator delegator = dispatcher.getDelegator();
-		String partyId = (String) context.get("partyId");
 		String appType = (String) context.get("appType");
-		String sendType = (String) context.get("seedType");
+		String sendType = (String) context.get("sendType");
 		String title = (String) context.get("title");
 		String content = (String) context.get("content");
-		Map<String, String> extras =  (Map<String, String>) context.get("extras");
+		String message = (String) context.get("message");
+		String partyId = (String) context.get("partyId");
 
 		// 查询registrationID
 		Map<String, Object> fields = FastMap.newInstance();
@@ -92,26 +94,55 @@ public class JPushServices {
 		//如果partyIdentifications大于0，获取regId,并推送消息
 		if(partyIdentifications.size() > 0){
 			JPushClient jPushClient = getJPushClient(delegator, appType);
-			String registrationID = partyIdentifications.get(0).toString();
+			String registrationID = partyIdentifications.get(0).getString("idValue");
 			PushPayload payload = null;
 			PushResult pushResult = null;
 			try {
 				if (("0").equals(sendType)) {
-					payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.registrationId(registrationID))
-							.setNotification(Notification.newBuilder()
-									.addPlatformNotification(
-											IosNotification.newBuilder().setAlert(content).addExtras(extras).build())
-									.addPlatformNotification(AndroidNotification.newBuilder().setAlert(content)
-											.setTitle(title).addExtras(extras).build())
-									.build())
-							.build();
+					if(UtilValidate.isNotEmpty(message) && UtilValidate.isNotEmpty(content)){
+						payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.registrationId(registrationID))
+								.setNotification(Notification.newBuilder()
+										.addPlatformNotification(
+												IosNotification.newBuilder().setAlert(content).setBadge(5).build())
+										.addPlatformNotification(AndroidNotification.newBuilder().setAlert(content)
+												.setTitle(title).build())
+										.build())
+								.setMessage(Message.newBuilder()
+				                        .setMsgContent(message)
+				                        .build())
+								.setOptions(Options.newBuilder()
+				                         .setApnsProduction(true)
+				                         .build())
+								.build();
+					}else if(UtilValidate.isNotEmpty(content) && UtilValidate.isEmpty(message)){
+						payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.registrationId(registrationID))
+								.setNotification(Notification.newBuilder()
+										.addPlatformNotification(
+												IosNotification.newBuilder().setAlert(content).setBadge(5).build())
+										.addPlatformNotification(AndroidNotification.newBuilder().setAlert(content)
+												.setTitle(title).build())
+										.build())
+								.setOptions(Options.newBuilder()
+				                         .setApnsProduction(true)
+				                         .build())
+								.build();
+					}else if(UtilValidate.isNotEmpty(message) && UtilValidate.isEmpty(content)){
+						payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.registrationId(registrationID))
+								.setMessage(Message.newBuilder()
+				                        .setMsgContent(message)
+				                        .build())
+								.setOptions(Options.newBuilder()
+				                         .setApnsProduction(true)
+				                         .build())
+								.build();
+					}
 				} else {
 					payload = PushPayload.alertAll(content);
 				}
-
+				
 				// 发送消息
 				pushResult = jPushClient.sendPush(payload);
-				
+
 				if (UtilValidate.isEmpty(pushResult)) {
 					Debug.logError(registrationID + ":推送消息失败:" + content, module);
 				}
