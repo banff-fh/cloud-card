d067ceccc3f81e65e7a710d9dd9f145ad1415d2a
subenkun <subenkun@gmail.com>
2017-04-11 13:54:25 +0800

    增加request方式上传图片

--- hot-deploy/cloudcard/src/com/banfftech/cloudcard/util/UtilFileUpload.java
+++ hot-deploy/cloudcard/src/com/banfftech/cloudcard/util/UtilFileUpload.java
@@ -4,10 +4,18 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilProperties;
@@ -17,6 +25,7 @@ import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.util.EntityUtilProperties;
 import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.service.ServiceUtil;
 
 import com.aliyun.oss.OSSClient;
@@ -97,6 +106,67 @@ public class UtilFileUpload {
         return result;
 
     }
+    
+    /**
+     * 上传文件，并返回图片路径及图片名称
+     *
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public static String uploadPictureWall(HttpServletRequest request, HttpServletResponse response)
+			throws GenericServiceException {
+		// Servlet Head
+		Delegator delegator = (Delegator) request.getAttribute("delegator");
+		HttpSession session = request.getSession();
+		String contentType = "";
+		
+		try {
+			ServletFileUpload dfu = new ServletFileUpload(new DiskFileItemFactory(10240, null));
+			List<FileItem> items = dfu.parseRequest(request);
+			int itemSize = 0;
+			if(null!=items){
+				itemSize = items.size();
+			}
+			
+			for (FileItem item : items) {
+				InputStream in = item.getInputStream();
+				String fileName = item.getName();
+				
+				String fileSuffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+				if (UtilValidate.isNotEmpty(fileSuffix)) {
+					GenericValue gv;
+					try {
+						gv = delegator.findOne("FileExtension", true, UtilMisc.toMap("fileExtensionId", fileSuffix.toLowerCase()));
+						if (gv != null){
+							contentType = gv.getString("mimeTypeId");
+						}
+					} catch (GenericEntityException e) {
+						Debug.logError(e.getMessage(), module);
+					}
+					
+				}
+				
+				if (fileName != null && (!fileName.trim().equals(""))) {
+		            // 创建OSSClient实例
+		            OSSClient client = getNewOssClient(delegator);
+		            ObjectMetadata objectMeta = new ObjectMetadata();
+                    objectMeta.setContentLength(item.getSize());
+                    // 可以在metadata中标记文件类型
+                    objectMeta.setContentType("image/jpeg");
+                    String key = UUID.randomUUID().toString() + System.currentTimeMillis();
+                    InputStream input = item.getInputStream();
+		            PutObjectResult pr = client.putObject(BUCKET_NAME, key, in, objectMeta);
+		            // pr 的结果需要判断下吧
+		            client.shutdown();
+				}
+			}
+		} catch (Exception e) {
+			Debug.logError(e.getMessage(), module);
+		}
+		return "success";
+	}
+
 
     /**
      * 删除oss上的文件
