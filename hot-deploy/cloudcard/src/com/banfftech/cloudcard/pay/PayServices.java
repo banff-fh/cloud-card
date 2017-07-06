80e25b2fea04b41167c8b125ab801a977a2196cc
developer <subenkun@gmail.com>
2017-07-06 15:11:43 +0800

    增加店铺等级过低用户不能充值的提示

--- hot-deploy/cloudcard/src/com/banfftech/cloudcard/pay/PayServices.java
+++ hot-deploy/cloudcard/src/com/banfftech/cloudcard/pay/PayServices.java
@@ -10,9 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.service.LocalDispatcher;
@@ -55,6 +58,24 @@ public class PayServices {
         }
 
         String storeId = cloudCard.getString("distributorPartyId");
+        //判断店家是否拥有用户充值的权限
+        EntityCondition dateCond = EntityUtil.getFilterByDateExpr();
+        EntityCondition cond = EntityCondition.makeCondition(UtilMisc.toMap("partyId", storeId,"partyClassificationTypeId","STORE_SALE_CLASSIFI"));
+        String storeSaleLevel = null;
+        try {
+			GenericValue partyClassification = EntityUtil.getFirst(
+			        delegator.findList("PartyClassificationAndPartyClassificationGroup", EntityCondition.makeCondition(cond, dateCond), null, UtilMisc.toList("-fromDate"), null, true));
+			if(UtilValidate.isNotEmpty(partyClassification)){
+				storeSaleLevel = partyClassification.getString("partyClassificationGroupId");
+			}
+        } catch (GenericEntityException e) {
+			Debug.logError(e.getMessage(), module);
+            return ServiceUtil.returnError(UtilProperties.getMessage(CloudCardConstant.resourceError, "CloudCardInternalServiceError", locale));
+		}
+
+        if(UtilValidate.isNotEmpty(storeSaleLevel) || storeSaleLevel.equalsIgnoreCase("STORE_SALE_LEVEL_1") ){
+            return ServiceUtil.returnError(UtilProperties.getMessage(CloudCardConstant.resourceError, "CloudCardShopLevelIsTooLow", locale));
+        }
 
         try {
             Map<String, Object> rechargeCloudCardReceiptOutMap = dispatcher.runSync("rechargeCloudCardReceipt", UtilMisc.toMap("userLogin", systemUserLogin,
@@ -71,7 +92,7 @@ public class PayServices {
             Debug.logError(e.getMessage(), module);
             return ServiceUtil.returnError(UtilProperties.getMessage(CloudCardConstant.resourceError, "CloudCardInternalServiceError", locale));
         }
-	    
+
 		Map<String, Object> result = FastMap.newInstance();
 		if(CloudCardConstant.PAY_CHANNEL_WXPAY.equals(paymentType)){
 			result = WeiXinPayServices.prepayOrder(dctx, context);
@@ -80,17 +101,17 @@ public class PayServices {
 		}
 		return result;
 	}
-	
+
 	//支付宝支付通知
 	public static void aliPayNotify(HttpServletRequest request,HttpServletResponse response){
 		AliPayServices.aliPayNotify(request, response);
 	}
-	
+
 	//微信支付通知
 	public static void wxPayNotify(HttpServletRequest request,HttpServletResponse response){
 		WeiXinPayServices.wxPayNotify(request, response);
 	}
-	
+
 	/**
 	 * 支付订单查询
 	 */
